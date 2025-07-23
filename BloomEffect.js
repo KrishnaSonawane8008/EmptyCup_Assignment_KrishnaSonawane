223ad022686dc19b04615fe2f4ca1e195967268a
KrishnaSonawane8008 <krishnasonawane1624@gmail.com>
2025-07-24 01:32:07 +0530

    Bloom effect update

---
+++ BloomEffect.js
@@ -0,0 +1,77 @@
+import * as THREE from "three";
+import {RenderPass} from 'jsm/postprocessing/RenderPass.js';
+import {EffectComposer} from 'jsm/postprocessing/EffectComposer.js';
+import {UnrealBloomPass} from 'jsm/postprocessing/UnrealBloomPass.js';
+import {OutputPass} from 'jsm/postprocessing/OutputPass.js';
+import {ShaderPass} from 'jsm/postprocessing/ShaderPass.js';
+
+
+
+
+function Bloom(scene, camera, renderer){
+    const renderScene= new RenderPass(scene, camera);
+    const BloomComposer=new EffectComposer(renderer);
+    const bloomPass=new UnrealBloomPass(
+        new THREE.Vector2(window.innerWidth,window.innerHeight),
+        1.6,
+        0.1,
+        0.1
+    );
+
+    renderer.toneMapping=THREE.LinearToneMapping;
+    renderer.toneMappingExposure=3;
+    renderer.outputColorSpace = THREE.SRGBColorSpace;
+
+
+    BloomComposer.addPass(renderScene);
+    BloomComposer.addPass(bloomPass)
+    bloomPass.strength = 1.5;
+    bloomPass.radius = 0.7;
+    bloomPass.threshold = 0.003;
+
+    BloomComposer.renderToScreen=false;
+
+    const mixPass=new ShaderPass(
+        new THREE.ShaderMaterial({
+            uniforms: {
+                baseTexture: {value: null},
+                bloomTexture: {value: BloomComposer.renderTarget2.texture}
+            },
+            vertexShader: document.getElementById('VertexShader').textContent,
+            fragmentShader: document.getElementById('FragmentShader').textContent
+        }), 'baseTexture'
+
+    );
+
+
+    const finalComposer = new EffectComposer(renderer);
+    finalComposer.addPass(renderScene);
+    finalComposer.addPass(mixPass);
+
+    const outputpass=new OutputPass();
+    finalComposer.addPass(outputpass)
+
+    return {BloomComposer, finalComposer}
+}
+
+const BLOOM_SCENE=1;
+const bloomLayer=new THREE.Layers();
+bloomLayer.set(BLOOM_SCENE);
+const darkMaterial= new THREE.MeshBasicMaterial({color:0x000000});
+const materials={};
+
+function nonBloomed(obj){
+    if(obj.isMesh && bloomLayer.test(obj.layers)==false){
+        materials[obj.uuid]=obj.material;
+        obj.material=darkMaterial;
+    }
+}
+
+function restoreMaterial(obj){
+    if(materials[obj.uuid]){
+        obj.material=materials[obj.uuid];
+        delete materials[obj.uuid];
+    }
+}
+
+export {Bloom, nonBloomed, restoreMaterial}
--- main.js
+++ main.js
@@ -1,12 +1,8 @@
 import * as THREE from "three";
 import {OrbitControls} from 'jsm/controls/OrbitControls.js';
 import {CreateSun, CreateEarthandMoon, CreateMercury, CreateVenus, CreateMars, CreateJupiter, CreateSaturn, CreateNeptune} from './PlanetCluster.js'
-import {RenderPass} from 'jsm/postprocessing/RenderPass.js';
-import {EffectComposer} from 'jsm/postprocessing/EffectComposer.js';
-import {UnrealBloomPass} from 'jsm/postprocessing/UnrealBloomPass.js';
-import {OutputPass} from 'jsm/postprocessing/OutputPass.js';
-import {ShaderPass} from 'jsm/postprocessing/ShaderPass.js';
 import datGui from 'https://cdn.jsdelivr.net/npm/dat.gui@0.7.9/+esm'
+import { Bloom, nonBloomed, restoreMaterial } from "./BloomEffect.js";
 
 const w=window.innerWidth;
 const h=window.innerHeight;
@@ -26,75 +22,13 @@ camera.position.y=50;
 const controls=new OrbitControls(camera, renderer.domElement);
 const scene=new THREE.Scene();
 
-const renderScene= new RenderPass(scene, camera);
-const BloomComposer=new EffectComposer(renderer);
 
-const bloomPass=new UnrealBloomPass(
-    new THREE.Vector2(window.innerWidth,window.innerHeight),
-    1.6,
-    0.1,
-    0.1
-);
+const Bloom_Effect=Bloom(scene, camera, renderer);
 
-renderer.toneMapping=THREE.LinearToneMapping;
-renderer.toneMappingExposure=3;
-renderer.outputColorSpace = THREE.SRGBColorSpace;
-
-
-BloomComposer.addPass(renderScene);
-BloomComposer.addPass(bloomPass)
-bloomPass.strength = 1.5;
-bloomPass.radius = 0.7;
-bloomPass.threshold = 0.003;
-
-BloomComposer.renderToScreen=false;
-
-const mixPass=new ShaderPass(
-    new THREE.ShaderMaterial({
-        uniforms: {
-            baseTexture: {value: null},
-            bloomTexture: {value: BloomComposer.renderTarget2.texture}
-        },
-        vertexShader: document.getElementById('VertexShader').textContent,
-        fragmentShader: document.getElementById('FragmentShader').textContent
-    }), 'baseTexture'
-
-);
 
 const gui= new datGui.GUI();
 
-const finalComposer = new EffectComposer(renderer);
-finalComposer.addPass(renderScene);
-finalComposer.addPass(mixPass);
 
-const outputpass=new OutputPass();
-finalComposer.addPass(outputpass)
-
-const BLOOM_SCENE=1;
-const bloomLayer=new THREE.Layers();
-bloomLayer.set(BLOOM_SCENE);
-const darkMaterial= new THREE.MeshBasicMaterial({color:0x000000});
-const materials={};
-
-function nonBloomed(obj){
-    if(obj.isMesh && bloomLayer.test(obj.layers)==false){
-        materials[obj.uuid]=obj.material;
-        obj.material=darkMaterial;
-    }
-}
-
-function restoreMaterial(obj){
-    if(materials[obj.uuid]){
-        obj.material=materials[obj.uuid];
-        delete materials[obj.uuid];
-    }
-}
-
-
-const ambientlight=new THREE.AmbientLight(0xffffff, 0.5)
-scene.add(ambientlight)
-const pointlight=new THREE.PointLight(0xffffff, 100);
-scene.add(pointlight)
 
 //=========================Planets===================================
 const SunMesh=CreateSun(4);
@@ -123,7 +57,9 @@ const Neptune_Orbit=CreateNeptune(40)
 scene.add(Neptune_Orbit)
 //=========================Planets===================================
 
-
+//=========================Lights====================================
+const ambientlight=new THREE.AmbientLight(0xffffff, 0.5)
+scene.add(ambientlight)
 
 // const pointlight=new THREE.PointLight(0xffffff, 100.0);
 // scene.add(pointlight)
@@ -131,7 +67,8 @@ scene.add(Neptune_Orbit)
 // const pointlight2=new THREE.PointLight(0xffffff, 1.0);
 // pointlight2.position.set(0,10,0)
 // scene.add(pointlight2)
-let runonce=true
+//=========================Lights====================================
+
 camera.layers.enableAll()
 function animate(){
     
@@ -146,20 +83,19 @@ function animate(){
     Saturn_Orbit.rotateY(-0.013)
     Neptune_Orbit.rotateY(-0.001)
 
-
 //==================================================================================
     scene.traverse(nonBloomed);
 
     //renderer.render(scene, camera);
-    BloomComposer.render();
+    Bloom_Effect.BloomComposer.render();
 
     scene.traverse(restoreMaterial);
 
     SunMesh.traverse((obj)=>{
-            obj.layers.set(BLOOM_SCENE)
+            obj.layers.set(1)//BLOOM_SCENE from './BloomEffect.js'
     })
     
-    finalComposer.render();
+    Bloom_Effect.finalComposer.render();
     
 
     requestAnimationFrame(animate);
